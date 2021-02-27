d318b0948c8e91a11bbee1ba9573a484b31676a2
Joel-Nwamba <79017478+Joel-Nwamb@users.noreply.github.com>
2021-02-26 21:56:37 -0500

    Creating Pokemon list
    I have added three main information about pokemon to compile a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-alert('Hello World!');
-let favoriteFood='pizza';
-document.write(favoriteFood);
+let pokemonList = [{
+    name: 'Bulbasaur',
+    height: 0.7,
+    types:['grass', 'poison']
+},
+{
+    name: 'Charizard',
+    height: 1.7,
+    types: ['Fire', 'Flying']
+},
+{
+    name: 'Pikachu',
+    height: 0.4,
+    types: ['Electric']
+}];
