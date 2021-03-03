bde9cd440e77dbe59d972b5b4ae5f309e1580fd7
Joel-Nwamba <79017478+Joel-Nwamb@users.noreply.github.com>
2021-03-03 15:41:48 -0500

    PoekmonList
    Set up  For loop, and condition for the Array.

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,14 @@ let pokemonList = [{
     height: 0.4,
     types: ['Electric']
 }];
+
+for(let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name +  ' (' +  'height:' + pokemonList[i].height + ' )');
+    if(pokemonList[i].height > 0.7 && pokemonList[i].height > 0.4){
+        document.write('  -' + 'Wow that is big!');
+    }
+  document.write('<br />' + '<br />')
+}
+
+
+
