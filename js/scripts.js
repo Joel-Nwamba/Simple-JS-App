e2b0d7875bf438bfaa84753fcd18a0cdc7bf8b9f
Joel-Nwamba <Joel.elyon.nwamba@gmail.com>
2021-03-11 11:49:58 -0500

    IIFE has been added

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
-let pokemonList = [{
-    name: 'Bulbasaur',
-    height: 0.7,
-    types:['grass', 'poison']
-},
-{
-    name: 'Charizard',
-    height: 1.7,
-    types: ['Fire', 'Flying']
-},
-{
-    name: 'Pikachu',
-    height: 0.4,
-    types: ['Electric']
-}];
+//IIFE for pokemonRepository
+let pokemonRepository = (function(){
+   let pokemonList = [{
+        name: 'Bulbasaur',
+        height: 0.7,
+        types:['grass', 'poison']
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['Fire', 'Flying']
+    },
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        types: ['Electric']
+    }];
 
-pokemonList.forEach(function(list){
+    function getAll() {
+        return pokemonList
+    }
+    function add(item){
+        pokemonList.push(item)
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+    
+
+})();
+
+pokemonRepository.getAll().forEach(function(list){
     document.write(list.name +  ' (' +  'height:' + list.height + ' )');
     if(list.height > 0.7 && list.height > 0.4){
         document.write('  -' + 'Wow that is big!');
     }
   document.write('<br />' + '<br />')
 })
+
+
 
 
 
