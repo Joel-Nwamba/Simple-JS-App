87252772436accb5161f1ac0473ee3703675f60d
Joel-Nwamba <Joel.elyon.nwamba@gmail.com>
2021-03-13 17:21:32 -0500

    DOM Interaction added

--- js/scripts.js
+++ js/scripts.js
@@ -22,21 +22,35 @@ let pokemonRepository = (function(){
     function add(item){
         pokemonList.push(item)
     }
-
+//This functions allows interaction to take place on the web.
+    function addListItem(pokemonOne){
+        let pokemon = document.querySelector('.pokemon-list');
+        let pokemonlist = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemonOne.name;
+        button.classList.add('class-button');
+        pokemon.appendChild(pokemonlist);
+        pokemonlist.appendChild(button);
+        button.addEventListener('click', function(event){
+            showDetails(pokemonOne);
+        })
+     }
+// Function is linked to AddListItem & called on addEventListner 'click'
+     function showDetails(pokemonOne) {
+        console.log(pokemonOne);
+     }
+// return is necessary in order push the resullts
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     }
     
 
 })();
-
-pokemonRepository.getAll().forEach(function(list){
-    document.write(list.name +  ' (' +  'height:' + list.height + ' )');
-    if(list.height > 0.7 && list.height > 0.4){
-        document.write('  -' + 'Wow that is big!');
-    }
-  document.write('<br />' + '<br />')
+// This generates the result from the code.
+pokemonRepository.getAll().forEach(function(pokemonOne){
+    pokemonRepository.addListItem(pokemonOne);
 })
 
 
