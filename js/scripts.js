fed0ce9b87f9976ea8364d515a332de5ee53fd76
Joel-Nwamba <Joel.elyon.nwamba@gmail.com>
2021-03-17 23:14:51 -0400

    Updated pokemon variable

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,34 @@
 let pokemonRepository = (function(){
    let pokemonList = [];
    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+   
+   function add(pokemon){
+    if (typeof pokemon === "object" &&
+    "name" in pokemon &&
+    "detailsUrl" in pokemon
+    ) {
+     pokemonList.push(pokemon);
+  } else {
+    console.log("pokemon is not correct");
+  } 
+    }
 
     function getAll() {
         return pokemonList
     }
-    function add(pokemon){
-        if (typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "detailsUrl" in pokemon
-        ) {
-        pokemonList.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-      } 
-        }
 
 
 //This functions allows interaction to take place on the web.
-    function addListItem(pokemonOne){
-        let pokemon = document.querySelector('.pokemon-list');
-        let pokemonlist = document.createElement('li');
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listpokemon = document.createElement('li');
         let button = document.createElement('button');
-        button.innerText = pokemonOne.name;
+        button.innerText = pokemon.name;
         button.classList.add('class-button');
-        pokemon.appendChild(pokemonlist);
-        pokemonlist.appendChild(button);
+        pokemonList.appendChild(pokemonlist);
+        listpokemon.appendChild(button);
         button.addEventListener('click', function(event){
-            showDetails(pokemonOne);
+            showDetails(pokemon);
         })
      }
      function loadList() {
@@ -36,11 +37,11 @@ let pokemonRepository = (function(){
             return response.json();
         }).then(function(json) {
            json.results.forEach(function(item){
-               let pokemonApp = {
+               let pokemon = {
                    name: item.name,
                    urlDetail: item.url
                }
-               add(pokemon)
+               add(pokemon);
                console.log(pokemon);
            });
         }).catch(function(e){
